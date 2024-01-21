eb8239e2918d59a8b608fee07ddb112472e50e65
jharr023 <jharringtonkc@gmail.com>
2024-01-21 15:15:22 -0600

    loops

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,14 +1,34 @@
+const input = require('readline-sync');
+
 let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
-
+let foodArray = food.split(",");
+console.log(foodArray.sort());
+let equipmentArray = equipment.split(",");
+console.log(equipmentArray.sort());
+let petsArray = pets.split(",");
+console.log(petsArray.sort());
+let sleepAidsArray = sleepAids.split(",");
+console.log(sleepAidsArray.sort());
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
-
+let cargoHold = [];
+cargoHold.push(foodArray);
+cargoHold.push(equipmentArray);
+cargoHold.push(petsArray);
+cargoHold.push(sleepAidsArray);
+console.log(cargoHold);
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
-
+let userInput = input.question("Pick a cabinet number: ");
+console.log(userInput);
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-
+if (userInput >= 0 && userInput <= 3){
+    let cabinetSelect = cargoHold.slice[userInput];
+    console.log(`Selected cabinet: ${(cabinetSelect)}`)
+} else {
+    console.log("Invalid Input.");
+}
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,11 +1,23 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
-
+console.log(str);
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+let strNew = str.split("Lau").reverse().join("Lau");
+console.log(strNew);
 //Use a template literal to print the original and modified string in a descriptive phrase.
-
+console.log(`The first phrase was ${str}, the modified phrase is ${strNew}`);
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-
+let userInput = input.question("Pick a number: ");
+console.log(userInput);
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if (userInput >= 0 && userInput <= 9){
+    let firstStr = str.slice(0, userInput);
+    let secondStr = str.slice(userInput);
+    console.log(secondStr + firstStr)
+} else {
+    userInput = 3;
+    let firstStr = str.slice(0, userInput);
+    let secondStr = str.slice(userInput);
+    console.log(`Invalid input. Defaulting to three characters: ${(secondStr + firstStr)}`);
+}

870c127031f82da2fa4d0bce2290ef7f8ef563c4
jharr023 <jharringtonkc@gmail.com>
2024-02-12 17:50:32 -0600

    commit

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,7 +1,13 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
+    let result;
+try {
+  reuslt = lab.runlab(3);
+} catch (error) {
+  result =  "Error thrown";
+}
+
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
@@ -22,3 +28,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);

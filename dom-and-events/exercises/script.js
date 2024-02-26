ce3432231c9f6388d6ed71565e3e8fa48e7d011a
jharr023 <jharringtonkc@gmail.com>
2024-02-26 17:34:20 -0600

    commit

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,17 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+ 
+     missionAbort.addEventListener("mouseover", function( event ) {
+        event.target.style.backgroundColor = "red";
+     });
+
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
 }
 
 window.addEventListener("load", init);
